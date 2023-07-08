93b3a416441ff95bfdbf43c875df5b37e5ec0110
Karuption <pederson26@gmail.com>
2023-07-08 17:14:28 -0500

    Project init

---
+++ Exams.NET/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.ApiAuthorization.IdentityServer;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using Duende.IdentityServer.EntityFramework.Options;
+using Exams.NET.Models;
+
+namespace Exams.NET.Data;
+
+public class ApplicationDbContext : ApiAuthorizationDbContext<ApplicationUser> {
+    public ApplicationDbContext(DbContextOptions options, IOptions<OperationalStoreOptions> operationalStoreOptions)
+        : base(options, operationalStoreOptions) { }
+}
---
+++ Exams.NET/Models/ApplicationUser.cs
@@ -0,0 +1,5 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace Exams.NET.Models;
+
+public class ApplicationUser : IdentityUser { }
