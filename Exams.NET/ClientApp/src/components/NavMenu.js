3725f2b7c0334f3c3ab9fc62bbfe52abc63f6d4a
Karuption <pederson26@gmail.com>
2023-08-27 16:21:29 -0500

    style: Nav bar restructure
    Nav bar now has an admin drop down and added portal link.

--- Exams.NET/ClientApp/src/components/NavMenu.js
+++ Exams.NET/ClientApp/src/components/NavMenu.js
@@ -1,53 +1,90 @@
-import React, { Component } from 'react';
-import { Collapse, Navbar, NavbarBrand, NavbarToggler, NavItem, NavLink } from 'reactstrap';
-import { Link } from 'react-router-dom';
-import { LoginMenu } from './api-authorization/LoginMenu';
-import './NavMenu.css';
+import React, { Component } from "react";
+import {
+   Collapse,
+   DropdownItem,
+   DropdownMenu,
+   DropdownToggle,
+   Navbar,
+   NavbarBrand,
+   NavbarToggler,
+   NavItem,
+   NavLink,
+   UncontrolledDropdown,
+} from "reactstrap";
+import { Link } from "react-router-dom";
+import { LoginMenu } from "./api-authorization/LoginMenu";
+import "./NavMenu.css";
 
 export class NavMenu extends Component {
-  static displayName = NavMenu.name;
+   static displayName = NavMenu.name;
 
-  constructor (props) {
-    super(props);
+   constructor(props) {
+      super(props);
 
-    this.toggleNavbar = this.toggleNavbar.bind(this);
-    this.state = {
-      collapsed: true
-    };
-  }
+      this.toggleNavbar = this.toggleNavbar.bind(this);
+      this.state = {
+         collapsed: true,
+      };
+   }
 
-  toggleNavbar () {
-    this.setState({
-      collapsed: !this.state.collapsed
-    });
-  }
+   toggleNavbar() {
+      this.setState({
+         collapsed: !this.state.collapsed,
+      });
+   }
 
-  render() {
-    return (
-      <header>
-        <Navbar className="navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3" container light>
-          <NavbarBrand tag={Link} to="/">Exams.NET</NavbarBrand>
-          <NavbarToggler onClick={this.toggleNavbar} className="mr-2" />
-          <Collapse className="d-sm-inline-flex flex-sm-row-reverse" isOpen={!this.state.collapsed} navbar>
-            <ul className="navbar-nav flex-grow">
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/">Home</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/counter">Counter</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/testAdmin">Test Administration</NavLink>
-              </NavItem>
-              <NavItem>
-                <NavLink tag={Link} className="text-dark" to="/questionAdmin">Question Administration</NavLink>
-              </NavItem>
-              <LoginMenu>
-              </LoginMenu>
-            </ul>
-          </Collapse>
-        </Navbar>
-      </header>
-    );
-  }
+   render() {
+      return (
+         <header>
+            <Navbar
+               className='navbar-expand-sm navbar-toggleable-sm ng-white border-bottom box-shadow mb-3'
+               container
+               light>
+               <NavbarBrand tag={Link} to='/'>
+                  Exams.NET
+               </NavbarBrand>
+               <NavbarToggler onClick={this.toggleNavbar} className='mr-2' />
+               <Collapse
+                  className='d-sm-inline-flex flex-sm-row-reverse'
+                  isOpen={!this.state.collapsed}
+                  navbar>
+                  <ul className='navbar-nav flex-grow'>
+                     <NavItem>
+                        <NavLink tag={Link} className='text-dark' to='/'>
+                           Home
+                        </NavLink>
+                     </NavItem>
+                     <NavItem>
+                        <NavLink tag={Link} className='text-dark' to='/Portal'>
+                           Portal
+                        </NavLink>
+                     </NavItem>
+                     <NavItem>
+                        <UncontrolledDropdown nav inNavbar>
+                           <DropdownToggle nav caret>
+                              Administration
+                           </DropdownToggle>
+                           <DropdownMenu right>
+                              <DropdownItem
+                                 tag={Link}
+                                 className='text-dark'
+                                 to='/testAdmin'>
+                                 Test Administration
+                              </DropdownItem>
+                              <DropdownItem
+                                 tag={Link}
+                                 className='text-dark'
+                                 to='/questionAdmin'>
+                                 Question Administration
+                              </DropdownItem>
+                           </DropdownMenu>
+                        </UncontrolledDropdown>{" "}
+                     </NavItem>
+                     <LoginMenu></LoginMenu>
+                  </ul>
+               </Collapse>
+            </Navbar>
+         </header>
+      );
+   }
 }
